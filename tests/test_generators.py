dccbd1822ac33154953e7e1bb9f8210359e4e62c
Elena Kaverina <kaverina0208.elena@gmail.com>
2026-02-22 14:50:39 +0300

    changes by flake8, black

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,41 +2,75 @@ import pytest
 from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 from typing import Any
 
+
 def test_filter_by_currency_USD(checklist_4: list) -> Any:
     n = filter_by_currency(checklist_4)
-    # assert  next(n) == {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572', 'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'}
-    assert  next(n) == {'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878', 'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'}
-    assert  next(n) == {'id': 895315941, 'state': 'EXECUTED', 'date': '2018-08-19T04:27:37.904916', 'operationAmount': {'amount': '56883.54', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод с карты на карту', 'from': 'Visa Classic 6831982476737658', 'to': 'Visa Platinum 8990922113665229'}
+    assert next(n) == {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    }
+    assert next(n) == {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    }
 
 
 def test_filter_by_currency_EUR(checklist_4: list) -> Any:
-    n = filter_by_currency(checklist_4, 'EUR')
-    assert  next(n) == 'Нет операций в такой валюте'
+    n = filter_by_currency(checklist_4, "EUR")
+    assert next(n) == "Нет операций в такой валюте"
 
 
 def test_filter_by_currency_name_missing(checklist_4: list) -> Any:
-    n = filter_by_currency(checklist_4, 'USD')
-    assert  next(n) == {'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878', 'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'}
-    assert  next(n) == {'id': 895315941, 'state': 'EXECUTED', 'date': '2018-08-19T04:27:37.904916', 'operationAmount': {'amount': '56883.54', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод с карты на карту', 'from': 'Visa Classic 6831982476737658', 'to': 'Visa Platinum 8990922113665229'}
+    n = filter_by_currency(checklist_4, "USD")
+    assert next(n) == {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    }
+    assert next(n) == {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    }
 
 
 def test_transaction_descriptions_correct(checklist_4: list) -> Any:
     n = transaction_descriptions(checklist_4)
-    assert next(n) == 'Перевод организации'
-    assert next(n) == 'Перевод со счета на счет'
-    assert next(n) == 'Перевод со счета на счет'
-    assert next(n) == 'Перевод с карты на карту'
-    assert next(n) == 'Перевод организации'
+    assert next(n) == "Перевод организации"
+    assert next(n) == "Перевод со счета на счет"
+    assert next(n) == "Перевод со счета на счет"
+    assert next(n) == "Перевод с карты на карту"
+    assert next(n) == "Перевод организации"
 
 
-@pytest.mark.parametrize("start, stop, expected_cards", [
-    (4000000000000000, 4000000000000002, ["4000 0000 0000 0000", "4000 0000 0000 0001", "4000 0000 0000 0002"]),
-    (4000000000000005, 4000000000000007, ["4000 0000 0000 0005", "4000 0000 0000 0006", "4000 0000 0000 0007"]),
-    (-3, 9, ['Введены некорректные данные']),
-    (3, -9, ['Введены некорректные данные']),
-    (9999999999999999, 9999999999999999, ['9999 9999 9999 9999'])
-])
-
+@pytest.mark.parametrize(
+    "start, stop, expected_cards",
+    [
+        (4000000000000000, 4000000000000002, ["4000 0000 0000 0000", "4000 0000 0000 0001", "4000 0000 0000 0002"]),
+        (4000000000000005, 4000000000000007, ["4000 0000 0000 0005", "4000 0000 0000 0006", "4000 0000 0000 0007"]),
+        (-3, 9, ["Введены некорректные данные"]),
+        (3, -9, ["Введены некорректные данные"]),
+        (9999999999999999, 9999999999999999, ["9999 9999 9999 9999"]),
+    ],
+)
 def test_card_number_generator(start: int, stop: int, expected_cards: list) -> Any:
     result = list(card_number_generator(start, stop))
     assert result == expected_cards
