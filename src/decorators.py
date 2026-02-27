3c50105330127c99ac4fd8c789d8ad9ae45b809d
Elena Kaverina <kaverina0208.elena@gmail.com>
2026-02-27 16:15:41 +0300

    changes from mypy, flake8, black

--- src/decorators.py
+++ src/decorators.py
@@ -1,36 +1,37 @@
-from time import time
 from functools import wraps
+from typing import Any, Union, Callable
 
 
-def log(*, filename=None):
-    def wrapper(func):
+def log(filename: Any = None) -> Any:
+    """The decorator can log the function's operation and its result to both a file and the console."""
+
+    def wrapper(func: Callable[..., Any]) -> Any:
         @wraps(func)
-        def inner(*args, **kwargs):
+        def inner(*args: Any, **kwargs: Any) -> Any:
             try:
-                time_1 = time()
                 result = func(*args, **kwargs)
-                time_2 = time()
                 if filename is None:
-                    print(f'{func.__name__} ok')
+                    print(f"{func.__name__} ok")
                 else:
-                    with open(filename, 'w', encoding='utf-8') as file:
-                        file.write(f'{func.__name__} ok')
+                    with open(filename, "w", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} ok")
                 return result
             except Exception as e:
                 if filename is None:
-                    print((f'{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}\n'))
+                    print(f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}\n")
                 else:
-                    with open(filename, 'w', encoding='utf-8') as file:
-                        file.write(f'{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}\n')
+                    with open(filename, "w", encoding="utf-8") as file:
+                        file.write(f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}\n")
                 raise
+
         return inner
+
     return wrapper
 
-#
-# @log(filename='mylog.txt')
-# def example(x, y):
-#     return x / y
-#
-#
-# example(10, 0)
-#
+
+@log()
+def example(x: Union[int, float], y: Union[int, float]) -> Any:
+    return x / y
+
+
+example(10, 2)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,18 +1,17 @@
 import pytest
+from typing import Union, Any
 from src.decorators import log
 
 
 @log()
-def func_console(x, y):
+def func_console(x: Union[int, float], y: Union[int, float]) -> Any:
     return x / y
 
 
-@pytest.mark.parametrize("x, y, expected", [
-    (3, 2, "func_console ok"),
-    (1, 0, "func_console error: ZeroDivisionError. Inputs: (1, 0), {}")
-])
-
-def test_log_to_console(capsys, x, y, expected):
+@pytest.mark.parametrize(
+    "x, y, expected", [(3, 2, "func_console ok"), (1, 0, "func_console error: ZeroDivisionError. Inputs: (1, 0), {}")]
+)
+def test_log_to_console(capsys: Any, x: Union[int, float], y: Union[int, float], expected: Any) -> Any:
     if y == 0:
         with pytest.raises(ZeroDivisionError):
             func_console(x, y)
@@ -23,17 +22,15 @@ def test_log_to_console(capsys, x, y, expected):
 
 
 @log(filename="test_log.txt")
-def func_file(x, y):
+def func_file(x: Union[int, float], y: Union[int, float]) -> Any:
     return x / y
 
 
-@pytest.mark.parametrize("x, y, expected", [
-    (3, 2, "func_file ok"),
-    (1, 0, "func_file error: ZeroDivisionError. Inputs: (1, 0), {}")
-])
-def test_log_to_file(x, y, expected):
+@pytest.mark.parametrize(
+    "x, y, expected", [(3, 2, "func_file ok"), (1, 0, "func_file error: ZeroDivisionError. Inputs: (1, 0), {}")]
+)
+def test_log_to_file(x: Union[int, float], y: Union[int, float], expected: Any) -> Any:
     log_file = "test_log.txt"
-
 
     if y == 0:
         with pytest.raises(ZeroDivisionError):
@@ -41,7 +38,6 @@ def test_log_to_file(x, y, expected):
     else:
         func_file(x, y)
 
-
-    with open(log_file, 'r') as f:
+    with open(log_file, "r") as f:
         content = f.read()
         assert expected in content
