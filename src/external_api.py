570bc187915811f7a955082c6b0eb4aecdaf3a8a
Elena Kaverina <kaverina0208.elena@gmail.com>
2026-04-04 00:33:54 +0300

    add .env.example

--- src/external_api.py
+++ src/external_api.py
@@ -1,18 +1,23 @@
 import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+APY_KEY = os.getenv('APY_KEY')
 
 def conversion_amount(transaction_data: dict) -> float:
     url = "https://api.apilayer.com/exchangerates_data/convert"
 
     headers = {
-        "apikey": "fkPegRUcYuoPK91U6iYpNHi2Gm980pi1"
-    }
+    'apikey': f'{APY_KEY}'
+}
 
     response = requests.get(url, headers=headers, params=transaction_data)
 
     if response.status_code != 200:
         raise ValueError(f"Не удалось получить курс валюты")
-    result = response.json()
+    result = response.json()['result']
     return result
 
 
